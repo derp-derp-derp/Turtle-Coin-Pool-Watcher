2df6b546229b0925e945adcc94bc2724f3c628a7
derp-derp-derp <derpes@protonmail.com>
2018-02-21 15:43:07 -0500

    dashboard alpha complete now includes highchart

--- pool-watcher/www/js/controllers.js
+++ pool-watcher/www/js/controllers.js
@@ -27,7 +27,7 @@ angular.module('tc.controllers', [])
     });
 }])
 
-.controller('DashboardCtrl', ['$scope', '$route', '$timeout', 'poolService', function DashboardCtrl($scope, $route, $timeout,  poolService) {
+.controller('DashboardCtrl', ['$scope', '$route', '$timeout', '$filter', 'poolService', function DashboardCtrl($scope, $route, $timeout, $filter,  poolService) {
     
     $scope.pool_api_url = $route.current.params.pool;
     $scope.wallet_address = $route.current.params.wallet_address;
@@ -37,6 +37,68 @@ angular.module('tc.controllers', [])
         $scope.miner_stats = stats.stats;
         $scope.paid_formatted = (Number(stats.stats.paid) / 100).toFixed(2);
         $scope.balance_formatted = (Number(stats.stats.balance) / 100).toFixed(2);
+        
+        var hashes = [];
+        
+        angular.forEach(stats.charts.hashrate, function(value, key) {
+            var data_point = {
+                name: stats.charts.hashrate[key][0],
+                y: Number(stats.charts.hashrate[key][1])
+            }
+            hashes.push(data_point);
+        });
+        
+        $('#chart').highcharts({
+            chart: {
+                height: 200,
+                backgroundColor: '#F9F9F9'
+            },
+            legend: {
+                enabled: false
+            },
+            credits: {
+                enabled: false
+            },
+            title: {
+                text: '',
+                style: {
+                    'display':'none'
+                }
+            },
+            yAxis: {
+                labels: {
+                  style: {
+                      fontFamily: '"Roboto", Helvetica, Arial',
+                      color: '#5d5d5d'
+                  }
+                },
+                title: {
+                    text: ''
+                },
+                gridLineColor: '#5d5d5d'
+            },
+            xAxis: {
+                labels: {
+                  enabled: false  
+                },
+                lineWidth: 0,
+                minorGridLineWidth: 0,
+                lineColor: 'transparent',
+                minorTickLength: 0,
+                tickLength: 0
+            },
+            tooltip: {
+                formatter: function(){
+                    return $filter('timeAgoLastShare')(this.point.name) + '<br>' + $filter('chartHashFormat')(this.point.y) + '/sec';
+                },
+                shadow: false
+            },
+            series: [{
+                color: '#00843d',
+                data: hashes
+            }]
+        });
+        
         $scope.loading = false;
     });
     
--- pool-watcher/www/js/filters.js
+++ pool-watcher/www/js/filters.js
@@ -7,4 +7,16 @@ angular.module('tc.filters', [])
     var d = new Date(parseInt(date) * 1000).toISOString();
     return jQuery.timeago(d);
   }
+})
+
+.filter('chartHashFormat', function() {
+  return function(hashrate) {
+    var i = 0;
+    var byteUnits = [' H', ' KH', ' MH', ' GH', ' TH', ' PH' ];
+    while (hashrate > 1000){
+        hashrate = hashrate / 1000;
+        i++;
+    }
+    return hashrate.toFixed(2) + byteUnits[i];
+  }
 });
