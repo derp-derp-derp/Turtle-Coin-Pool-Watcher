8bc505570ab50f2403b45bd9fab7bd9c9b6dc4c3
derp-derp-derp <derpes@protonmail.com>
2018-02-21 00:14:56 -0500

    Adds timeago for human readable last share time

--- pool-watcher/www/js/filters.js
+++ pool-watcher/www/js/filters.js
@@ -2,16 +2,9 @@
 
 angular.module('tc.filters', [])
 
-.filter('getReadableHashRateString', ['$filter', function ($filter) {
-    return function (hashrate) {
-		var i = 0;
-		var byteUnits = [' H', ' KH', ' MH', ' GH', ' TH', ' PH' ];
-
-		while (hashrate > 1000) {
-			hashrate = hashrate / 1000;
-			i++;
-		}
-
-        return hashrate.toFixed(2) + byteUnits[i];
-    };
-}]);
+.filter('timeAgoLastShare', function() {
+  return function(date) {
+    var d = new Date(parseInt(date) * 1000).toISOString();
+    return jQuery.timeago(d);
+  }
+});
